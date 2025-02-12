8d37997a2b10d06df9d4fe496eb335d12594c1f7
mahsa <mkmi7171@gmail.com>
2025-02-12 14:02:00 -0500

    user list testing completed

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,12 +1,20 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
+function renderComponent() {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+  render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
 test('render one row per user', () => {
-    const users = [
-        { name: 'jane', email: 'jane@jane.com' },
-        {name:'sam', email:'sam@sam.com'},
-    ]
-   render(<UserList users={users} />);
+  renderComponent();
 
     const rows = within(screen.getByTestId('users')).getAllByRole('row')
 
@@ -14,11 +22,7 @@ test('render one row per user', () => {
 })
 
 test('render the email and name of each user', () => {
-    const users = [
-        { name: 'jane', email: 'jane@jane.com' },
-        {name:'sam', email:'sam@sam.com'},
-    ]
-   render(<UserList users={users} />);
+    const {users } = renderComponent();
   for (let user of users) {
     const name = screen.getByRole('cell', { name: user.name });
     const email = screen.getByRole('cell', { name: user.email });
